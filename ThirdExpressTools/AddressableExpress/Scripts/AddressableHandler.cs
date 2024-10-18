0ee9124f18f8e1b32a677bc9263ef621597f0263
hxd-hxd <404987771@qq.com>
2024-10-18 18:17:41 +0800

    1、整理程序集结构 2、AddressableExpress 扩展

---
+++ ThirdExpressTools/AddressableExpress/Scripts/AddressableHandler.cs
@@ -0,0 +1,28 @@
+﻿// -------------------------
+// 创建日期：2024/10/14 14:53:10
+// -------------------------
+
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Framework;
+using UnityEngine.AddressableAssets;
+
+namespace Framework.AddressableExpress
+{
+    public class AddressableHandler : IResourcesHandler
+    {
+        public T GetObject<T>(string path) where T : UnityEngine.Object
+        {
+            //Addressables.DownloadDependenciesAsync
+            return null;
+        }
+
+        public UnityEngine.Object GetObject(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

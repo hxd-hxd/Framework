74642067cb592eec02cdec12f2e744193141a3b4
hxd-hxd <404987771@qq.com>
2026-04-24 11:40:22 +0800

    修改：增加导出多个文件到unitypackage的功能

--- Editor/Scripts/Other/ExportUnitypackage.cs
+++ Editor/Scripts/Other/ExportUnitypackage.cs
@@ -31,7 +31,7 @@ namespace Framework.Editor
         //    AssetDatabase.ExportPackage(fs, outPath);
         //}
 
-        [MenuItem(Root_Menu + "导出到当前文件夹（不包含依赖）", false, Root_MenuIndex)]
+        [MenuItem(Root_Menu + "快速导出到当前文件夹（不包含依赖）", false, Root_MenuIndex)]
         public static void ExportToCurrentDir()
         {
             var obj = Selection.activeObject;
@@ -47,6 +47,96 @@ namespace Framework.Editor
                 var uPack = AssetDatabase.LoadAssetAtPath<Object>(outPath);
                 Selection.activeObject = uPack;
                 AssetDatabase.Refresh();
+            }
+        }
+
+        [MenuItem(Root_Menu + "导出选中的所有文件到当前文件夹（不包含依赖）", false, Root_MenuIndex)]
+        public static void ExportSelectAllToCurrentDir()
+        {
+            var obj = Selection.activeObject;
+
+            if (obj)
+            {
+                var f = AssetDatabase.GetAssetPath(obj);
+                string packName = Path.GetFileNameWithoutExtension(Path.GetFileName(f));
+
+                // 获取选中的所有文件
+                //Selection.assetGUIDs.ToList().ForEach(guid =>
+                //{
+                //    var path = AssetDatabase.GUIDToAssetPath(guid);
+                //    Debug.Log($"选择的资产路径: {path}");
+                //});
+                var selectAssets = Selection.assetGUIDs.Select((guid, indexer) =>
+                {
+                    var path = AssetDatabase.GUIDToAssetPath(guid);
+                    return path;
+                }).ToArray();
+
+                // 用户自定义包名
+                var popup = new UserPackNamePopupWindow(packName, _ExportPackageFunc);
+                Rect buttonRect = default;
+                buttonRect.position = new Vector2(Screen.width / 2, Screen.height / 2);
+                buttonRect.size = popup.GetWindowSize();
+                PopupWindow.Show(buttonRect, popup);
+
+                //_ExportPackageFunc(packName);
+
+                void _ExportPackageFunc(string packName)
+                {
+                    string outPath = $"{Path.GetDirectoryName(f)}/{packName}{Export_Expanded_Name}";
+                    AssetDatabase.ExportPackage(selectAssets, outPath, ExportPackageOptions.Recurse);
+
+                    AssetDatabase.Refresh();
+
+                    var uPack = AssetDatabase.LoadAssetAtPath<Object>(outPath);
+                    Selection.activeObject = uPack;
+                    AssetDatabase.Refresh();
+                }
+            }
+        }
+
+        public class UserPackNamePopupWindow : PopupWindowContent
+        {
+            private string packName;
+            public Action<string> onPackNameEntered;
+
+            public UserPackNamePopupWindow(string defaultPackName, Action<string> onPackNameEntered)
+            {
+                packName = defaultPackName;
+                this.onPackNameEntered = onPackNameEntered;
+            }
+            
+            public override Vector2 GetWindowSize()
+            {
+                return new Vector2(200, 100);
+            }
+
+            public override void OnGUI(Rect rect)
+            {
+                Init();
+
+                GUILayout.Label("请输入包名:");
+                packName = GUILayout.TextField(packName);
+                if (GUILayout.Button("导出"))
+                {
+                    // 处理确定按钮点击事件
+                    // 例如，获取输入的包名并执行导出操作
+                    onPackNameEntered?.Invoke(packName);
+                    this.editorWindow.Close();
+                }
+            }
+
+            bool isInitialized = false;
+            private void Init()
+            {
+                if (!isInitialized)
+                {
+                    isInitialized = true;
+                    //var pos = editorWindow.position;
+                    //pos.position = UnityEngine.Event.current.mousePosition;
+                    ////pos.position = GUILayoutUtility.GetLastRect().position;
+                    //editorWindow.position = pos;
+                }
             }
         }
     }

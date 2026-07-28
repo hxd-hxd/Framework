04cd6a980202002cfc2e04a15d1f9335295eb8dd
hxd-hxd <404987771@qq.com>
2026-07-28 14:40:44 +0800

    修改：扩展列表功能

--- Scripts/Core/Utility/ListExtend.cs
+++ Scripts/Core/Utility/ListExtend.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 namespace Framework.Core
 {
     /// <summary>
-    /// <see cref="IList"/> 扩展
+    /// <see cref="IList"/>、<see cref="List{T}"/> 扩展
     /// </summary>
     public static class ListExtend
     {
@@ -52,6 +52,84 @@ namespace Framework.Core
             self[targetIndex] = sourceItem;
 
             return result;
+        }
+
+        /// <summary>调整列表大小到指定大小，多余的则移除，不够则创建</summary>
+        public static void AdjustNew<T>(this List<T> values, int count) where T : new()
+        {
+            if (count < 0) return;
+
+            while (values.Count != count)
+            {
+                if (values.Count > count)
+                {
+                    values.RemoveAt(values.Count - 1);
+                }
+                else if (values.Count < count)
+                {
+                    values.Add(new());
+                }
+            }
+        }
+
+        /// <summary>调整列表大小到指定大小，多余的则移除，不够则使用默认值</summary>
+        public static void AdjustDefault<T>(this List<T> values, int count) where T : new()
+        {
+            if (count < 0) return;
+
+            while (values.Count != count)
+            {
+                if (values.Count > count)
+                {
+                    values.RemoveAt(values.Count - 1);
+                }
+                else if (values.Count < count)
+                {
+                    values.Add(default);
+                }
+            }
+        }
+
+        /// <summary>调整列表大小到指定大小，多余的则移除，不够则创建</summary>
+        public static void Adjust<T>(this List<T> values, int count, Action<T, int> callback) where T : new()
+        {
+            if (count < 0) return;
+
+            while (values.Count != count)
+            {
+                if (values.Count > count)
+                {
+                    var obj = values[values.Count - 1];
+                    values.RemoveAt(values.Count - 1);
+                    callback?.Invoke(obj, -1);
+                }
+                else if (values.Count < count)
+                {
+                    T obj = new();
+                    values.Add(obj);
+                    callback?.Invoke(obj, 1);
+                }
+            }
+        }
+
+        /// <summary>调整列表大小到指定大小，多余的则移除，不够则创建</summary>
+        public static void Adjust<T>(this List<T> values, int count, Func<T> newCallback)
+        {
+            if (count < 0) return;
+
+            while (values.Count != count)
+            {
+                if (values.Count > count)
+                {
+                    values.RemoveAt(values.Count - 1);
+                }
+                else if (values.Count < count)
+                {
+                    T obj = newCallback.Invoke() ?? default;
+                    values.Add(obj);
+                    
+                }
+            }
         }
     }
 }

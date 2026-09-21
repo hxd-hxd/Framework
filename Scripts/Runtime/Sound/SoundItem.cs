7543cf318a342db9fae6c2e0ae1c41b2b49fb9b9
hxd-hxd <404987771@qq.com>
2026-09-21 17:15:47 +0800

    调整：本地化预制体文件夹结构 添加：声音模块部分定义

---
+++ Scripts/Runtime/Sound/SoundItem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Framework.Sound
+{
+    /// <summary>声音项</summary>
+    public class SoundItem : ISoundItem
+    {
+        
+    }
+}
